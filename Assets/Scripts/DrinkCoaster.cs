25a72ef58d58a2dc4e7a2f102f38cb61bc082796
Robert Guan <guan.robert@utexas.edu>
2020-11-22 17:01:07 -0600

    hooking up more functions in scene

--- Assets/Scripts/DrinkCoaster.cs
+++ Assets/Scripts/DrinkCoaster.cs
@@ -6,6 +6,7 @@ namespace Assets.Scripts
 {
     public enum BeeState
     {
+        MovingToSeat,
         Ordering,
         Waiting,
         Drinking
@@ -14,35 +15,71 @@ namespace Assets.Scripts
     {   
         public GameObject drinkHeld;
         public GameObject beePrefab;
-        public GameObject beeSpawn;
         public Cup cup;
         private Vector3 cupSpawnLocation;
         public float cupSpawnVerticalOffset = .00115f;
         public Ingredient curIngredient;
         private bool beeSpawned;
-
+        public Recipes menu;
+        private List<Ingredient> menuItems;
+        private Ingredient beesOrder;
+        private GameObject currentBee;
+        private BeeState curState;
+        public GameObject beeSpawn;
+        public Vector3 beeSpawnPos;
+        public GameObject seat;
+        private Vector3 seatPos;
+        public Vector3 moveVelocity = new Vector3(1,1,1);
+        public float dampTime = 2f;
+        void Start()
+        {
+            menuItems = menu.getEndIngredients();
+            seatPos = seat.transform.position;
+            beeSpawnPos = beeSpawn.transform.position;
+        }
         public void deliverDrink()
         {
             curIngredient = cup.currentDrink;
             cup.ResetCup();
+            
             drinkHeld = Instantiate(curIngredient.Model, cupSpawnLocation, transform.rotation) as GameObject;
             drinkHeld.transform.parent = transform;
         }
 
+        private Ingredient randomMenuItem()
+        {
+            return menuItems[Random.Range(0,menuItems.Count - 1)];
+        }
         private IEnumerator beeLife()
         {
-            SpawnBee();
-            yield return null;
-            StartCoroutine(beeLife());
+            yield return spawnBee();
+            yield return beeOrder();
+            if(!beeSpawned){
+                StartCoroutine(beeLife());
+            }
+            
         }
         private IEnumerator beeOrder()
         {
+            beesOrder = randomMenuItem();
             yield return null;
         }
 
-        private void SpawnBee()
+        private IEnumerator spawnBee()
         {
-            
+            currentBee = Instantiate(beePrefab, beeSpawnPos, beeSpawn.transform.rotation) as GameObject;
+            currentBee.transform.parent = transform;
+            curState = BeeState.MovingToSeat;
+            yield return moveBeeToSeat();
+        }
+
+        private IEnumerator moveBeeToSeat()
+        {
+            while(Vector3.Distance(currentBee.transform.position, seatPos) > .1f){
+                currentBee.transform.position = Vector3.SmoothDamp(currentBee.transform.position, seatPos, ref moveVelocity, dampTime);
+                yield return null;
+            }
+            yield return beeOrder();
         }
     }
 }
