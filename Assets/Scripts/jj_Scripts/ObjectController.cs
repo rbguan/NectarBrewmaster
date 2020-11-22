1cb625470c4afec947784152720285c8b65f40b4
Jeshua Johnson <jwj845@austin.eid.utexas.edu>
2020-11-22 02:03:44 -0600

    + Building VR functionality in jj_scene and prefabs

---
+++ Assets/Scripts/jj_Scripts/ObjectController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Gvr.Internal;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectController : MonoBehaviour, IPointerEnterHandler
+{
+
+	public bool gazedAt = false;
+
+	bool gazzing;
+    
+    void Start(){
+    	
+    }
+
+
+    void Update()
+    {
+        if(gazzing){
+
+        }
+    }
+
+    public void GazeStart(){
+    	bool gazzing = true;
+
+    }
+
+    public void GazeEnd(){
+    	bool gazzing = false;
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+    	Debug.Log("I have this.");
+    }
+
+}
